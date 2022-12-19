8928bc04ca879f1abab8918bf9ecf60eaea6e9a0
Ivan <icabulo@gmail.com>
2022-12-19 15:58:31 -0500

    primer commit proyecto testimonials

---
+++ src/componentes/Testimonio.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import "../stylesheets/Testimonio.css";
+
+function Testimonio(props) {
+  return (
+    <div className="contenedor-testimonio">
+      <img
+        src={`./src/imagenes/testimonio-${props.imagen}.png`}
+        className="imagen-testimonio"
+        alt={`Foto de ${props.imagen}`}
+      />
+      <div className="contenedor-texto-testimonio">
+        <p className="nombre-testimonio">
+          <strong>{props.nombre}</strong> en {props.pais}
+        </p>
+        <p className="cargo testimonio">
+          {props.cargo} en <strong>{props.empresa}</strong>
+        </p>
+        <p className="texto-testimonio">{props.testimonio}</p>
+      </div>
+    </div>
+  );
+}
+
+export default Testimonio;
